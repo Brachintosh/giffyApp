d5e198b2c2a24bf0379d7215b4587468c479fc28
brachintosh <bedendo.br@gmail.com>
2022-05-11 22:46:40 -0300

    style-anchor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ListOfGifs from './components/ListOfGifs';
 import './App.css';
-import { Route } from 'wouter';
+import { Route, Link } from 'wouter';
 
 function App() {
 
   return (
     <div className="App">
       <section className='App-content'>
-        <h3>GiffyApp</h3>
-        <a href='/gif/panda' >Giffs de Pandas</a>
-        <a href='/gif/brasil' >Giffs de Brasil</a>
-        <a href='/gif/argentina' >Giffs de Argentina</a>
-        <a href='/gif/matrix' >Giffs de Matrix</a>
+        <Link to='/'>
+          <h3 className='app-title' >GiffyApp</h3>
+        </Link>
+        <Link to='/gif/panda' >Giffs de Pandas</Link>
+        <Link to='/gif/brasil' >Giffs de Brasil</Link>
+        <Link to='/gif/argentina' >Giffs de Argentina</Link>
+        <Link to='/gif/matrix' >Giffs de Matrix</Link>
         <Route path='/gif/:keyword' component={ListOfGifs} />
-        {/* <ListOfGifs keyword='brasil' /> */}
-
       </section>
     </div>
   );
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,6 +1,6 @@
 const apiKey='0vXAlCjt4QqgQXLO2l81XTQbZ6XX4vFA';
 
-export default function getGifs({keyword = 'morty'} = {} ) {
+export default function getGifs({keyword} = {} ) {
     const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
     
     return fetch(apiURL)
