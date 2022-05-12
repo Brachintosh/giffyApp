08697c9eefd81c12bef5906f95dbf642eaf1d6bf
brachintosh <bedendo.br@gmail.com>
2022-05-12 17:12:30 -0300

    lastSearch

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,16 +1,21 @@
 import { useEffect, useState } from 'react';
 import getGifs from '../services/getGifs';
 
-export default function useGifs({ keyword }){
+export default function useGifs( { keyword } = { keyword: null } ){
     const [loading, setLoading] = useState(false);
     const [gifs, setGifs] = useState([]);
 
     useEffect(function() {
         setLoading(true)
-        getGifs({ keyword })
+        // Reading the last keyword at localStorage:
+        const keywordToUse  = keyword || localStorage.getItem('lastKeyword') || 'random';
+        console.log("soy keywordToUse >> ", keywordToUse);
+        getGifs({ keyword: keywordToUse })
             .then(gifs => {
                 setGifs(gifs);
                 setTimeout(() => setLoading(false), 2500);
+                // Saving the keyword at localStorage
+                localStorage.setItem('lastKeyword', keyword);
             });
     }, [keyword]);
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "wouter";
+import useGifs from "../../hooks/useGifs";
+import ListOfGifs from "../../components/ListOfGifs";
 
 const POPULAR_GIFS = ['Pandas', 'Argentina', 'Brasil', 'Matrix', ];
 
 export default function Home() {
     const [keyword, setKeyword] = useState('');
     const [path, pushLocation] = useLocation();
-    // console.log('location :>> ', location);
+    const {loading, gifs} = useGifs();
 
     const handleSubmit = (e) => {
         e.preventDefault();
         //  Navigate to route
-        // console.log('keyword :>> ', keyword);
         pushLocation(`/search/:${keyword}`);
     }
 
@@ -25,7 +26,8 @@ export default function Home() {
                 <input placeholder="Search for a gif..." onChange={handleInput} type='text' value={keyword} />
                 <button>Search!</button>
             </form>
-
+            <h3 className="App-title" >⤗ Ésta es la última búsqueda:</h3>
+            <ListOfGifs gifs={gifs} loading={loading} />
             <h3 className="App-title" >⤗ Éstos son los Giffs más vistos:</h3>
             
             <ul>
